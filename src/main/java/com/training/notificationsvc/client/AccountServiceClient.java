51f201cdfb76506c46cec19f00d5e651b8517fd1
Mushtaq Ahmed <amushtaq@vmware.com>
2022-01-25 15:03:21 +0530

    Initial Circuit breaker
    Signed-off-by: Abhishek Sawhney <sawhneyab@vmware.com>

--- src/main/java/com/training/notificationsvc/client/AccountServiceClient.java
+++ src/main/java/com/training/notificationsvc/client/AccountServiceClient.java
@@ -1,13 +1,38 @@
 package com.training.notificationsvc.client;
 
+import java.math.BigDecimal;
+
+import com.sun.jna.platform.win32.Advapi32Util.Account;
 import com.training.notificationsvc.dto.TransactionDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "account-service", url = "${account-service-url}")
+@FeignClient(name = "account-service", url = "${account-service-url}", fallback = AccountServiceClient.AccountServiceClientFallback.class )
 public interface AccountServiceClient {
 
     @GetMapping("/api/transactions/{transactionId}")
     TransactionDto getTransaction(@PathVariable long transactionId);
+
+	@Component
+	static class AccountServiceClientFallback implements AccountServiceClient{
+
+		@Override
+		public TransactionDto getTransaction(long transactionId) {
+
+			System.out.println("Fallback triggered: Returning dummy transactionDto");
+
+			TransactionDto dummy = new TransactionDto();
+			dummy.setAccountId(null);
+			dummy.setTransactionId(transactionId);
+			dummy.setAmount(BigDecimal.ZERO);
+			dummy.setBalance(BigDecimal.ZERO);
+			dummy.setDescription("");
+			dummy.setCreateAt("");
+
+			return dummy;
+		}
+	}
+
 }
